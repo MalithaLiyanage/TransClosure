e9bc78c9674169ce8254c48a28be8ffd3e86aa99
Liyanage Malitha <e15299@ce.pdn.ac.lk>
2019-12-09 20:37:19 +0530

    FindNeighbours function added

--- Transitive_Closure.c
+++ Transitive_Closure.c
@@ -19,3 +19,33 @@ int adMatrix[7][7] = {
 int transClosure[7][7];
 
 int count = 0;
+
+
+
+
+void findNeighbours(int x)
+{
+    int i;
+    for (i = 0; i < 7; i++)
+    {
+        if (adMatrix[x-1][i] == 1)
+        {
+            next[nextIndex] = i+1;
+            nextIndex++;
+            count++;
+        }
+        
+        if (nextIndex >9)
+        {
+            return;
+        }
+        
+        //printf("Next is %d\n", nextIndex);
+    }
+}
+
+int main()
+{
+	
+	return 0;
+}
